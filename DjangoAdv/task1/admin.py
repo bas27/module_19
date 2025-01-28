ee767d569bb5a222ff47ba7b74d3b6ef5852854a
Alexander B <bas2784@gmail.com>
2025-01-28 12:31:11 +0300

    добавлен Сайт администрирования

--- DjangoAdv/task1/admin.py
+++ DjangoAdv/task1/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Buyer, Game
+
 
 # Register your models here.
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'balance')
+    search_fields = ('name',)
+    list_filter = ('balance', 'age')
+    list_per_page = 30
+    readonly_fields = ('balance',)
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')
+    search_fields = ('title',)
+    list_filter = ('size', 'cost')
+    list_per_page = 20
--- DjangoAdv/task1/models.py
+++ DjangoAdv/task1/models.py
@@ -4,22 +4,26 @@ from django.db import models
 
 
 class Buyer(models.Model):
-    name = models.CharField(max_length=100)
-    balance = models.DecimalField(max_digits=10, decimal_places=2)
-    age = models.IntegerField()
+    name = models.CharField(max_length=100, verbose_name="Логин")
+    balance = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Баланс")
+    age = models.IntegerField(verbose_name="Возраст")
 
     def __str__(self):
         return (f'Имя: {self.name}\n'
                 f'Баланс: {self.balance}\n'
                 f'Возраст: {self.age}')
 
+    class Meta:
+        verbose_name = "Покупатель"
+        verbose_name_plural = "Покупатели"
+
 
 class Game(models.Model):
-    title = models.CharField(max_length=100)
-    cost = models.DecimalField(max_digits=10, decimal_places=2)
-    size = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField()
-    age_limited = models.BooleanField(default=False)
+    title = models.CharField(max_length=100, verbose_name="Наименование")
+    cost = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    size = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Размер")
+    description = models.TextField(verbose_name="Описание")
+    age_limited = models.BooleanField(default=False, verbose_name="Возрастное ограничение")
     buyer = models.ManyToManyField(Buyer, related_name='games')
 
     def __str__(self):
@@ -28,3 +32,7 @@ class Game(models.Model):
                 f'Размер: {self.size}\n'
                 f'Описание: {self.description}\n'
                 f'Возрастной рейтинг: {self.age_limited}')
+
+    class Meta:
+        verbose_name = "Игра"
+        verbose_name_plural = "Игры"
